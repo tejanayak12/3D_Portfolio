ab83d9e4ca5283c4e964b4ae814fa7a7159a0c8d
tejanayak12 <118418860+tejanayak12@users.noreply.github.com>
2025-06-12 20:13:16 +0530

    HERO 3D SECTION COMPLETED

---
+++ portfolio/src/sections/Hero.jsx
@@ -0,0 +1,60 @@
+import Button from "../components/Button"
+import HeroExperience from "../components/HeroModels/HeroExperience"
+import { words } from "../constants"
+
+
+const Hero = () => {
+    return (
+        <section id='hero' className='relative overflow-hidden'>
+            <div className='absolute top-0 left-0 z-10'>
+                <img src='/images/bg.png' alt='background' />
+            </div>
+
+            <div className='hero-layout'>
+                {/** left side : HERO content */}
+                <header className='flex flex-col justify-center md:w-full w-screen md:px-20 px-5'>
+                    <div className='flex flex-col gap-7'>
+                        <div className='hero-text'>
+                            <h1>
+                                Shaping
+                                <span className='slide'>
+                                    <span className='wrapper'>
+                                        {words.map((word) => (
+                                            <span key={word.text} className='flex items-center md:gap-3 gap-1 pb-2'>
+                                                <img
+                                                    src={word.imgPath}
+                                                    alt={word.text}
+                                                    className='xl:size-12 md:size-10 size-7 md:p-2 p-1 rounded-full bg-white-50'
+                                                />
+
+                                                <span>{word.text}</span>
+                                            </span>
+                                        ))}
+                                    </span>
+                                </span>
+                            </h1>
+                            <h1>into real Projects</h1>
+                            <h1>that deliver results</h1>
+                        </div>
+                        <p className="text-white-50 md:text-xl relative z-10 pointer-events-none">
+                            Hi, I'm Teja Naik a Visionary Frontend Developer
+                        </p>
+                        <Button
+                            className="md:w-80 md:h-16 w-60 h-12"
+                            id="button"
+                            text="See My Work"
+                        />
+                    </div>
+                </header>
+                {/** right side : 3d model */}
+                <figure>
+                    <div className="hero-3d-layout border-red-200">
+                        <HeroExperience />
+                    </div>
+                </figure>
+            </div>
+        </section>
+    )
+}
+
+export default Hero
