6d9a778cec53ccbeb0aa2c644b3eea6f00eb292a
tejanayak12 <118418860+tejanayak12@users.noreply.github.com>
2025-06-18 22:50:29 +0530

    Nav Bar Completed

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,9 +1,11 @@
+import NavBar from "./components/NavBar"
 import Hero from "./sections/Hero"
 import ShowCaseSection from "./sections/ShowCaseSection"
 
 const App = () => {
     return (
         <>
+            <NavBar />
             <Hero />
             <ShowCaseSection />
         </>
